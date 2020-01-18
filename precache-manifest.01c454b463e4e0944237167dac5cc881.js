dd24ef5c7bea076295006b6cdc6ee75fbf02608d
Andrej2035 <53010831+Andrej2035@users.noreply.github.com>
2020-01-18 12:16:02 +0200

    Updates

---
+++ precache-manifest.01c454b463e4e0944237167dac5cc881.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8048d5bfe20f4214276eb1dd31dbe3c0",
+    "url": "/magazin/index.html"
+  },
+  {
+    "revision": "b838dacef276f9f6e58f",
+    "url": "/magazin/static/css/10.864625eb.chunk.css"
+  },
+  {
+    "revision": "80dd813ddb6672b63c57",
+    "url": "/magazin/static/css/4.bf87b20c.chunk.css"
+  },
+  {
+    "revision": "aa95b6922ee3ffd49417",
+    "url": "/magazin/static/css/5.7b9f36c6.chunk.css"
+  },
+  {
+    "revision": "e7d203f944b7c3622791",
+    "url": "/magazin/static/css/6.6df5debf.chunk.css"
+  },
+  {
+    "revision": "d0be8674523db173d62f",
+    "url": "/magazin/static/css/7.268a0542.chunk.css"
+  },
+  {
+    "revision": "fa2b7b28af31264f42b9",
+    "url": "/magazin/static/css/8.4a182094.chunk.css"
+  },
+  {
+    "revision": "103947f59734141dee61",
+    "url": "/magazin/static/css/9.7ca79506.chunk.css"
+  },
+  {
+    "revision": "708c6a96420c84ce308e",
+    "url": "/magazin/static/css/main.f383bb0f.chunk.css"
+  },
+  {
+    "revision": "b838dacef276f9f6e58f",
+    "url": "/magazin/static/js/10.a8205b1a.chunk.js"
+  },
+  {
+    "revision": "86e3f815c6310d9411fd",
+    "url": "/magazin/static/js/2.b4970268.chunk.js"
+  },
+  {
+    "revision": "c26ea03cf61dea7499ba",
+    "url": "/magazin/static/js/3.488a4879.chunk.js"
+  },
+  {
+    "revision": "80dd813ddb6672b63c57",
+    "url": "/magazin/static/js/4.d92bd5ab.chunk.js"
+  },
+  {
+    "revision": "aa95b6922ee3ffd49417",
+    "url": "/magazin/static/js/5.a919ed0e.chunk.js"
+  },
+  {
+    "revision": "e7d203f944b7c3622791",
+    "url": "/magazin/static/js/6.7ae03cea.chunk.js"
+  },
+  {
+    "revision": "d0be8674523db173d62f",
+    "url": "/magazin/static/js/7.5f625610.chunk.js"
+  },
+  {
+    "revision": "fa2b7b28af31264f42b9",
+    "url": "/magazin/static/js/8.578167e2.chunk.js"
+  },
+  {
+    "revision": "103947f59734141dee61",
+    "url": "/magazin/static/js/9.3a362a2b.chunk.js"
+  },
+  {
+    "revision": "708c6a96420c84ce308e",
+    "url": "/magazin/static/js/main.9728260d.chunk.js"
+  },
+  {
+    "revision": "7b95428283fe63b9f80d",
+    "url": "/magazin/static/js/runtime~main.d319fcc9.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/magazin/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "ca13928888458625bc1d5fe51c72a90d",
+    "url": "/magazin/static/media/preloader.ca139288.gif"
+  }
+]);
